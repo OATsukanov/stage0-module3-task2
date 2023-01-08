fd0fdf70f59afd8dc7d9f7adad0d646267861ca3
oleg <tsukanov.regula@gmail.com>
2023-01-09 00:50:35 +0500

    first try

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,13 +2,13 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 0;
-        int variable = 2000;
-        int value = 1000;
-        int variable = 4000;
-        int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
-        int value = 90000;
+        int firstVariable = 0;
+        int secondVariable = 2000;
+        int firstValue = 1000;
+        int thirdVariable = 4000;
+        int secondValue = 3000;
+        int myVariables = 5000;
+        int yourVariables = 7000;
+        int otherValue = 90000;
     }
 }
